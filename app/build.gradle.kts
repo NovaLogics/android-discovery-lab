c2d38829cfbc69ea0250b09ef41325356d4bb908
NovaLogics <rajitha.shavinda@gmail.com>
2025-02-04 19:30:14 +0530

    config(gradle.kts): update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -50,15 +53,56 @@ android {
 }
 
 dependencies {
-
+    //Architecture
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.swipe.refresh.layout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    //Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigate.compose)
+    implementation(libs.androidx.hilt.compose)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.animation)
+    implementation(libs.androidx.foundation)
+    //Room Database
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    //OkHttp
+    implementation(libs.okHttp)
+    //Gson
+    implementation(libs.gson)
+    //Coil
+    implementation(libs.coil)
+    //Coroutines
+    implementation(libs.coroutine.core)
+    implementation(libs.coroutine.android)
+    //Material
+    implementation(libs.material)
+    //Hilt
+    implementation(libs.dagger.android)
+    ksp(libs.dagger.compiler)
+    ksp(libs.hilt.compiler)
+    //Palette
+    implementation(libs.androidx.palette.ktx)
+    //Splashscreen
+    implementation(libs.androidx.core.splashscreen)
+    //Accompanist Pager Indicators
+    implementation(libs.accompanist.pager.indicators)
+    //DataStore
+    implementation(libs.datastore.preferences)
+
+    //Unit Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
